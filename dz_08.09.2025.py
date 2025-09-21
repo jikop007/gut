8a70aef114f2c0df52925cbf2038a78b39564314
Titov Zakhar <jikop007@gmail.com>
2025-09-21 12:05:06 +0300

    fixed: '1*0+1', '1 + 2'; s -> symbol; S -> strOfNumAndOper; list of operation turns to four 'elif' that fix the 1*0+1; add ''.replace to fix '1 + 2'

--- dz_08.09.2025.py
+++ dz_08.09.2025.py
@@ -1,32 +1,60 @@
-def SortStation(S):
+def sortStation(strOfNumAndOper):
+    
+    strOfNumAndOper = strOfNumAndOper.replace(' ', '')
+    
     precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
     output, stack = [], []
-    for s in S:
-        if s.isdigit():
-            output.append(s)
-        elif s == '(':
-            stack.append(s)
-        elif s == ')':
+    
+    newStrOfNumAndOper = []
+    i = 0
+    
+    while i < len(strOfNumAndOper):
+        if strOfNumAndOper[i].isdigit():
+            num = strOfNumAndOper[i]
+            while i+1 < len(strOfNumAndOper) and strOfNumAndOper[i+1].isdigit():
+                i += 1
+                num += strOfNumAndOper[i]
+            newStrOfNumAndOper.append(num)
+        else:
+            newStrOfNumAndOper.append(strOfNumAndOper[i])
+        i += 1
+    
+    for symbol in newStrOfNumAndOper:
+        if symbol.isdigit():
+            output.append(symbol)
+        elif symbol == '(':
+            stack.append(symbol)
+        elif symbol == ')':
             while stack and stack[-1] != '(':
                 output.append(stack.pop())
             stack.pop()
         else:
-            while stack and stack[-1] != '(' and precedence[s] <= precedence[stack[-1]]:
+            while stack and stack[-1] != '(' and precedence[symbol] <= precedence[stack[-1]]:
                 output.append(stack.pop())
-            stack.append(s)
+            stack.append(symbol)
+            
     return output + stack[::-1]
 
-def Reading(S):
+def reading(strOfNumAndOper):
+    
     stack = []
-    for s in S:
-        if s in '+-*/^':
+    
+    for symbol in strOfNumAndOper:
+        if symbol in '+-*/^':
             b = stack.pop()
             a = stack.pop()
-            stack.append({'+': a+b, '-': a-b, '*': a*b, '/': a/b, '^': a**b}[s])
+            
+            if symbol == '/' and b == 0:
+                return "/0"
+            if symbol == '+': stack.append(a + b)
+            elif symbol == '-': stack.append(a - b)
+            elif symbol == '*': stack.append(a * b)
+            elif symbol == '/': stack.append(a / b if b != 0 else float('inf'))
+            elif symbol == '^': stack.append(a ** b)
         else:
-            stack.append(float(s))
+            stack.append(float(symbol))
+    
     return stack[0]
     
 s = str(input())
-print(SortStation(s))
-print(Reading(SortStation(s)))
+print(reading(sortStation(s)))
